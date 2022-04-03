9bd4977f7a8751b8b9bb041dac7364898814eb1a
verGe <a1988a1@yandex.ru>
2022-04-03 22:15:50 +0300

    add files

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,29 @@
 from abc import ABC
+from exceptions import LowFuelError, NotEnoughFuel, CargoOverload
+#from car import Car
 
 
 class Vehicle(ABC):
-    pass
+
+    def __init__(self, weight = 1500, fuel = 100, fuel_consumption = 10):
+        self.weight = weight
+        self.started = False
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if self.fuel > 0:
+            self.started = True
+        else:
+            raise LowFuelError(self.fuel)
+
+    def move(self, distance):
+        fuel_consumption_calc = self.fuel_consumption * distance
+        if self.fuel > fuel_consumption_calc:
+            self.fuel = self.fuel - fuel_consumption_calc
+        else:
+            raise NotEnoughFuel(fuel_consumption_calc)
+
+#new_vehicle = Vehicle(weight=1500, fuel=100, fuel_consumption=5)
+#new_vehicle.start()
+#new_vehicle.move(100)
--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,12 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+
+from base import Vehicle
+
+class Car(Vehicle):
+    pass
+
+
+new_car = Car(weight=1500, fuel=100, fuel_consumption=10)
+print(vars(new_car))
