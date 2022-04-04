88642f36610fb722bc28ba5918e87ebff2450a15
verGe <a1988a1@yandex.ru>
2022-04-04 20:59:41 +0300

    add files

--- homework_02/car.py
+++ homework_02/car.py
@@ -2,13 +2,11 @@
 создайте класс `Car`, наследник `Vehicle`
 """
 from homework_02 import base
-from homework_02 import Engine
-
 class Car(base.Vehicle):
 
     def __init__(self, weight = 1500, fuel = 100, fuel_consumption = 10):
         super().__init__(weight, fuel, fuel_consumption)
         self.engine = None
 
-    def set_engine(self, class_engine: Engine):
+    def set_engine(self, class_engine: object):
         self.engine = class_engine
