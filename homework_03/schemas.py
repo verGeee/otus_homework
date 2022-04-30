c0b99d88b1330a82359ea9e26704cb48f4ebfb08
verGe <a1988a1@yandex.ru>
2022-05-01 00:00:55 +0300

    add email

--- homework_03/schemas.py
+++ homework_03/schemas.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, constr
 
 class UserBase(BaseModel):
     username: constr(min_length=8)
+    email: str = "default@default"
 
 
 class UserIn(UserBase):
@@ -12,6 +13,7 @@ class UserIn(UserBase):
 class UserOut(UserBase):
     id: int
     created_at: str
+
 
 class User(UserOut):
     pass
