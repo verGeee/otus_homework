fcde13cd34b10074f5158a4bccbd1c5fda5b2fc5
verGe <a1988a1@yandex.ru>
2022-05-25 20:56:14 +0300

    hw4

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,6 +1,29 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
+import asyncio, aiohttp, json
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+import platform
+
+if platform.system() == "Windows":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/posts/1"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+async def get_user_data():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(USERS_DATA_URL) as resp:
+            return await resp.json()
+
+
+async def post_user_data():
+    async with aiohttp.ClientSession() as session:
+        async with session.post(POSTS_DATA_URL) as resp:
+            print(await resp.json())
+
+
+if __name__ == "__main__":
+    asyncio.run(get_user_data())
