a6084f8b46e9d769d52cc12ab3f8ce79eb76a05e
verGe <a1988a1@yandex.ru>
2022-05-28 20:15:01 +0300

    hw4

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -2,6 +2,7 @@
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
 import asyncio, aiohttp, json
+from dataclasses import dataclass
 
 import platform
 
@@ -9,21 +10,65 @@ if platform.system() == "Windows":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
-USERS_DATA_URL = "https://jsonplaceholder.typicode.com/posts/1"
-POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+@dataclass
+class Services:
+    name: str
+    url: str
 
 
-async def get_user_data():
+@dataclass
+class UsersData(Services):
+    field_name: str
+    field_username: str
+    field_email: str
+
+
+@dataclass
+class PostsData(Services):
+    field_user_id: int
+    field_title: str
+    field_body: str
+
+
+USERS_DATA_URL = [
+    UsersData(
+        name="users",
+        url="https://jsonplaceholder.typicode.com/users",
+        field_name="name",
+        field_username="username",
+        field_email="email",
+    ),
+    PostsData(
+        name="posts",
+        url="https://jsonplaceholder.typicode.com/posts",
+        field_user_id="userId",
+        field_title="title",
+        field_body="body",
+    ),
+]
+
+
+async def fetch_json(service: Services) -> dict:
     async with aiohttp.ClientSession() as session:
-        async with session.get(USERS_DATA_URL) as resp:
-            return await resp.json()
+        async with session.get(service.url) as resp:
+            data: dict = await resp.json()
+            if service.name == 'users':
+                return (data[0].get(service.field_name))
+            elif service.name == 'posts':
+                pass
+            return data
 
 
-async def post_user_data():
-    async with aiohttp.ClientSession() as session:
-        async with session.post(POSTS_DATA_URL) as resp:
-            return await resp.json()
+# async def get_data() -> dict:
+#     data_dict = {}
+#     tasks = {asyncio.create_task(fetch_json(url)) for url in USERS_DATA_URL}
+#     coro = asyncio.wait(tasks)
+#     done, pending = await coro
+#
+#     for task in done:
+#         data_dict = task.result()
+#         # print(data_dict)
 
 
-if __name__ == "__main__":
-    pass
+data = asyncio.run(fetch_json(USERS_DATA_URL[0]))
+print(data)
--- homework_04/models.py
+++ homework_04/models.py
@@ -9,8 +9,89 @@
 """
 
 import os
+from sqlalchemy import (
+    Table,
+    create_engine,
+    Column,
+    Integer,
+    String,
+)
+from sqlalchemy.orm import (
+    declarative_base,
+    sessionmaker,
+    scoped_session,
+    Session as SessionType,
+)
 
-PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+# PG_CONN_URI = (
+#     os.environ.get("SQLALCHEMY_PG_CONN_URI")
+#     or "postgresql+asyncpg://postgres:password@localhost/postgres"
+# )
 
-Base = None
-Session = None
+
+PG_CONN_URI = "postgresql+pg8000://hw4:hw4@localhost:5432/hw4"
+
+engine = create_engine(url=PG_CONN_URI)
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
+
+class Base:
+    id = Column(Integer, primary_key=True)
+    def __str__(self):
+        return (
+            f"id={self.id}, "
+            f"username={self.username!r}, "
+            f"name={self.name!r}, "
+            f"email={self.email!r} "
+        )
+
+    def __repr__(self):
+        return str(self)
+
+
+Base = declarative_base(bind=engine, cls=Base)
+
+
+class User(Base):
+    __tablename__ = "User"
+    name = Column("name", String)
+    username = Column("username", String, unique=True)
+    email = Column("email", String, unique=True)
+
+class Post(Base):
+    __tablename__ = "Post"
+    user_id = Column("user_id", Integer, unique=True)
+    title = Column("title", String)
+    body = Column("body", String)
+
+
+def create_table():
+    Base.metadata.create_all()
+
+# def get_users(session: SessionType) -> list[User]:
+#     users = session.query(User).all()
+#     print("users:", users)
+#     return users
+
+
+# def create_user(session: SessionType, username: str, name: str, email: str) -> User:
+#     username = User(username=username, name=name, email=email)
+#     session.add(username)
+#     session.commit()
+#     return username
+
+
+# def change_user(session: SessionType, user: User) -> User:
+#     user.email = "new_email"
+#     session.commit()
+#     return user
+
+session: SessionType = Session()
+create_table()
+# get_users(session)
+# create_user(session, username="vergee", name="pash", email="verge@email.com")
+# change_user(session, username='test', name='test_name', email='test_email')
+session.close()
+
+
