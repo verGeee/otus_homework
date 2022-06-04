57cda09488d6b907d29c4f254ae5d6fca13102a6
verGe <a1988a1@yandex.ru>
2022-06-05 01:11:48 +0300

    hw5

--- homework_05/app.py
+++ homework_05/app.py
@@ -10,3 +10,40 @@
 в базовый шаблон добавьте навигационную панель nav (https://getbootstrap.com/docs/5.0/components/navbar/)
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
+
+from flask import (
+    Flask,
+    render_template,
+)
+
+app = Flask(__name__)
+app.config.update(ENV="development")
+
+pages_dict = {1: "Smth_else", 2: "About"}
+
+
+@app.route("/")
+def get_pages():
+    return render_template(
+        "index.html",
+        pages=list(pages_dict.items()),
+    )
+
+
+@app.get("/<int:page_id>/")
+def get_page_id(page_id: int):
+    page_name = pages_dict[page_id]
+    return render_template("details.html", page_name=page_name)
+
+
+# @app.route("/about/")
+# def about():
+#     return render_template("about.html")
+#
+#
+# @app.route("/smth_else/")
+# def smth_else():
+#     return render_template("smth_else.html")
+
+
+app.run(debug=True)
