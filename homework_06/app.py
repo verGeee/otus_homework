8849498db4f0567666be6f58808c00605645b260
verGe <a1988a1@yandex.ru>
2022-06-19 12:51:56 +0300

    change docker-compose.yaml and app

--- homework_06/app.py
+++ homework_06/app.py
@@ -4,26 +4,25 @@ from models.database import db
 from flask_migrate import Migrate
 
 
-shop = Flask(__name__)
-shop.config.update(
+app = Flask(__name__)
+app.config.update(
     ENV="development",
     SECRET_KEY="dadadadad",
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
     SQLALCHEMY_DATABASE_URI="postgresql+pg8000://shop:shop@localhost:5432/shop",
 )
 
-shop.register_blueprint(products_app, url_prefix="/products")
-
-db.init_app(shop)
-migrate = Migrate(shop, db, compare_type=True)
-
+app.register_blueprint(products_app, url_prefix="/products")
+db.init_app(app)
+migrate = Migrate(app, db, compare_type=True)
 
 
 
-@shop.route("/", endpoint="home")
+
+@app.route("/", endpoint="home")
 def home_page():
     return render_template("home.html", products=get_products_on_home())
 
 
 if __name__ == "__main__":
-    shop.run(debug=True)
+    app.run(debug=True)
