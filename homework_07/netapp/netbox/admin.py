404bc9a919fc003e8e13ab7c1f53d7ff5de3f13d
verGe <a1988a1@yandex.ru>
2022-07-03 22:18:52 +0300

    hw7

--- homework_07/netapp/netbox/admin.py
+++ homework_07/netapp/netbox/admin.py
@@ -4,8 +4,8 @@ from .models import Switch, Router, Vendor
 
 
 class CommonAdminData(admin.ModelAdmin):
-    list_display = ("id", 'name', "manufactured", "model_name", "serial_number")
-    list_display_links = ("id", 'name', "model_name")
+    list_display = ("id", "name", "manufactured", "model_name", "serial_number")
+    list_display_links = ("id", "name", "model_name")
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
