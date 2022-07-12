b1962116078ac739e169f1081695bc3df0f6481c
verGe <a1988a1@yandex.ru>
2022-07-12 18:57:12 +0300

    hw8

--- homework_08/netapp/netbox/urls.py
+++ homework_08/netapp/netbox/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from .views import (
-    details,
     DeviceListView,
+    SiteListView,
+    SiteDetailView,
+    DeviceDetailView,
 )
 
 app_name = "netbox"
 
 urlpatterns = [
     path("", DeviceListView.as_view(), name="list"),
-    path("<int:pk>/", details, name="details"),
+    path("detail/<int:pk>/", DeviceDetailView.as_view(), name="details"),
+    path("site/", SiteListView.as_view(), name="site_list"),
+    path("site/<str:site>/", SiteDetailView.as_view(), name="site"),
 ]
--- homework_08/netapp/netbox/views.py
+++ homework_08/netapp/netbox/views.py
@@ -1,34 +1,41 @@
 from django.shortcuts import (
-    render,
     get_object_or_404,
 )
-from django.http import HttpRequest
+
 from django.views.generic import (
     ListView,
-    FormView,
+    DetailView,
 )
 
+from .models import (
+    Device,
+    Site,
+)
 
-from .models import Device
-
-
-def index(requset: HttpRequest):
-    items = Device.objects.select_related("manufactured").order_by("name").all()
-    context = {
-        "items": items,
-    }
-
-    return render(requset, "netbox/index.html", context=context)
-
-
-def details(requset: HttpRequest, pk: int):
-    items = get_object_or_404(Device.objects.select_related("manufactured"), pk=pk)
-    context = {
-        "item": items,
-    }
-    return render(requset, "netbox/details.html", context=context)
-
+from django.db import models
 
 class DeviceListView(ListView):
     queryset = Device.objects.select_related("manufactured").order_by("name")
     context_object_name = "items"
+
+
+class SiteListView(ListView):
+    queryset = Site.objects.order_by("site")
+    context_object_name = "items"
+
+class SiteDetailView(ListView):
+    queryset = Device.objects
+    context_object_name = "items"
+    template_name = "netbox/site.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        site: Site = get_object_or_404(Site, site=self.kwargs["site"])
+        return qs.filter(site__id=site.id)
+
+class DeviceDetailView(DetailView):
+    queryset = Device.objects.select_related("site", "manufactured", "dev_type")
+    context_object_name = "item"
+    template_name = "netbox/details.html"
+
+
